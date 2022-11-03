11e071ec6998e503f2adcbf606a730645483b7c7
maghfirafiolife <117273629+maghfirafiolife@users.noreply.github.com>
2022-11-03 09:10:04 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public Main {
+    public static void main(String[] args) {
+        //////////////////0  1   2  3 4  5  6
+        int[] intArray = {20,35,-15,7,55,1,-22};
+
+        for(int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex > 0; lastUnsortedIndex--
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
+    public static void swap (int[] array, int i, int j){
+        if(i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
